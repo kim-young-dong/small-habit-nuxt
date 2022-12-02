54ff0636ca00b8b7deca99a77851ae042088cbf8
Ming <77654432+kim-young-dong@users.noreply.github.com>
2022-12-02 19:32:51 +0900

    WIP

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,19 +1,29 @@
 <script setup>
+  //'https://kauth.kakao.com/oauth/authorize?client_id=795931948b0147ba49bbcd17e7943ed2&redirect_uri=http://localhost:3001/loading&response_type=code',
   const router = useRouter()
   async function kakaoLogin() {
-    await navigateTo(
-      {
-        path: 'https://kauth.kakao.com/oauth/authorize',
-        query: {
-          client_id: '795931948b0147ba49bbcd17e7943ed2',
-          redirect_uri: 'http://localhost:3001/loading',
-          response_type: 'code',
-        },
+    router.push({
+      path: 'https://kauth.kakao.com/oauth/authorize',
+      query: {
+        client_id: '795931948b0147ba49bbcd17e7943ed2',
+        redirect_uri: 'http://localhost:3001/loading',
+        response_type: 'code',
       },
-      {
-        external: true,
-      },
-    )
+    })
+    // await navigateTo(
+    //   {
+    //     path: 'https://google.com',
+    //     query: {
+    //       page: 10,
+    //       client_id: '795931948b0147ba49bbcd17e7943ed2',
+    //       redirect_uri: 'http://localhost:3001/loading',
+    //       response_type: 'code',
+    //     },
+    //   },
+    //   {
+    //     external: true,
+    //   },
+    // )
   }
 </script>
 
